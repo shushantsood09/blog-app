29ae841666022b7307b56eebb15ba468242b5f2f
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2023-06-05 19:59:47 +0530

    basic routing and bootstrap installation setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostsComponent } from './pages/single-posts/single-posts.component';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'category', component: SingleCategoryComponent },
+  { path: 'post', component: SinglePostsComponent },
+  { path: 'tems-conditions', component: TermsAndConditionsComponent },
+  { path: 'contact', component: ContactUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
