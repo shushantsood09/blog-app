d0f1cfe225ddda68b9491ca7d65d9af64df54a99
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2023-06-05 18:37:04 +0530

    created components for the app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostsComponent } from './pages/single-posts/single-posts.component';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
+import { CommentFormComponent } from './comments/comment-form/comment-form.component';
+import { CommentListComponent } from './comments/comment-list/comment-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoryNavbarComponent,
+    HomeComponent,
+    SingleCategoryComponent,
+    SinglePostsComponent,
+    TermsAndConditionsComponent,
+    ContactUsComponent,
+    SubscriptionFormComponent,
+    CommentFormComponent,
+    CommentListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/single-posts/single-posts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-posts',
+  templateUrl: './single-posts.component.html',
+  styleUrls: ['./single-posts.component.css']
+})
+export class SinglePostsComponent {
+
+}
